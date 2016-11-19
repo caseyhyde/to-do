2dc4dde7aa433549601a86517c6226554af0a44c
Casey Hyde <hyde.casey@gmail.com>
2016-11-19 14:54:09 -0600

    Installed and set up dependencies for express, jquery, pg, & body-parser

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var tasks = require('./routes/')
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('./public/views/index.html'));
+});
+
+app.use(express.static('./public'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ', app.get('port'));
+});
---
+++ server/routes/tasks.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/sigma';
