97855c44cf1313e72cadacafd9b5d4661a53e9af
Casey Hyde <hyde.casey@gmail.com>
2016-11-21 09:23:46 -0600

    changes to sql

--- database.sql
+++ database.sql
@@ -1,20 +1,21 @@
-CREATE TABLE current_tasks (
+CREATE TABLE tasks (
   id SERIAL PRIMARY KEY,
   task_name VARCHAR(50),
   task_details VARCHAR(1000)
+  completes BOOLEAN DEFAULT FALSE
  );
 
- CREATE TABLE completed_tasks (
-   id SERIAL PRIMARY KEY,
-   task_name VARCHAR(50),
-   task_details VARCHAR(1000),
- );
-
- CREATE TABLE future_tasks (
-   id SERIAL PRIMARY KEY,
-   task_name VARCHAR(50),
-   task_details VARCHAR(1000)
- );
+ -- CREATE TABLE completed_tasks (
+ --   id SERIAL PRIMARY KEY,
+ --   task_name VARCHAR(50),
+ --   task_details VARCHAR(1000),
+ -- );
+ --
+ -- CREATE TABLE future_tasks (
+ --   id SERIAL PRIMARY KEY,
+ --   task_name VARCHAR(50),
+ --   task_details VARCHAR(1000)
+ -- );
 
  INSERT INTO task (task_name, task_details)
  VALUES('Walk the dog', 'Fluffy needs a 1 mile walk!'),
